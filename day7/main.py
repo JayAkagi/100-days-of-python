7db94cac25208e530a99a5ac2f63d1cba757aca4
JayAkagi <b019782k@gmail.com>
2023-12-07 05:31:05 +0000

    Hangman pt4

--- day7/main.py
+++ day7/main.py
@@ -1,18 +1,94 @@
 import random
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+
 word_list = ["daedalus", "butterfly", "bracer", "broadsword"]
 display = []
 
 selected_word = word_list[random.randint(0, len(word_list) - 1)]
+player_lives = 6
+win = False
 
-user_guess = input("Guess a letter: ").lower()
-
-for blank in range(len(selected_word)):
+for i in range(len(selected_word)):
     display.append("_")
 
-for index in range(len(selected_word)):
-    char = selected_word[index]
-    if char == user_guess:
-        display[index] = char
+while player_lives != 0:
+    print(stages[player_lives])
+    print(player_lives)
+    print(f"Complete the word: {''.join(display)}")
+    user_guess = input("Guess a letter: ").lower()
+    turn_ok = False
 
-print(f" Complete the word: {''.join(display)}")
+    for i in range(len(selected_word)):
+        char = selected_word[i]
+        if char == user_guess:
+            display[i] = char
+            turn_ok = True
+
+    if not turn_ok:
+        player_lives -= 1
+
+if "_" not in display:
+    win = "True"
+
+if win:
+    print("You win!")
+else:
+    print(stages[player_lives])
+    print(f"You Lose. The word was: {selected_word}")
