26b5b1ad5f3e8938c1e6f48045650e210e5ac769
JayAkagi <b019782k@gmail.com>
2023-12-07 03:14:31 +0000

    Hangman pt1

--- day7/main.py
+++ day7/main.py
@@ -4,7 +4,7 @@ word_list = ["daedalus", "butterfly", "bracer", "broadsword"]
 
 selected_word = word_list[random.randint(0, len(word_list) - 1)]
 
-user_guess = input("Guess a letter: ")
+user_guess = input("Guess a letter: ").lower()
 
 if user_guess in selected_word:
     print("Letter is in the word.")
