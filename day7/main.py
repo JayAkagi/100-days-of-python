928d1dfb33ccc86440fdf5037c4ea2ac98c3b0fd
JayAkagi <b019782k@gmail.com>
2023-12-07 06:02:00 +0000

    Hangman

--- day7/main.py
+++ day7/main.py
@@ -1,78 +1,30 @@
 import random
+import os
 
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+import hangman_art
+import hangman_words
 
-
-word_list = ["daedalus", "butterfly", "bracer", "broadsword"]
+word_list = hangman_words.word_list
 display = []
 
-selected_word = word_list[random.randint(0, len(word_list) - 1)]
+selected_word = hangman_words.word_list[random.randint(0, len(word_list) - 1)]
 player_lives = 6
 win = False
 
 for i in range(len(selected_word)):
     display.append("_")
 
+print(hangman_art.logo)
+
 while player_lives != 0:
-    print(stages[player_lives])
-    print(player_lives)
+    print(hangman_art.stages[player_lives])
     print(f"Complete the word: {''.join(display)}")
     user_guess = input("Guess a letter: ").lower()
+
+    if user_guess in display:
+        print(f'\n\n\n\nYou already guessed "{user_guess}"')
+
+
     turn_ok = False
 
     for i in range(len(selected_word)):
@@ -82,6 +34,7 @@ while player_lives != 0:
             turn_ok = True
 
     if not turn_ok:
+        print(f'\n\n\n\nYou guessed a letter that is not in the word. You lose a life.')
         player_lives -= 1
 
 if "_" not in display:
@@ -90,5 +43,5 @@ if "_" not in display:
 if win:
     print("You win!")
 else:
-    print(stages[player_lives])
+    print(hangman_art.stages[player_lives])
     print(f"You Lose. The word was: {selected_word}")
