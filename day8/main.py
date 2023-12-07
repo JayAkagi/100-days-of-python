2841da04acba9d71624ee7d683c7d1d92ecd8bd0
JayAkagi <b019782k@gmail.com>
2023-12-07 22:11:24 +0000

    Caesar cipher 0.2

--- day8/main.py
+++ day8/main.py
@@ -1,7 +1,7 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
@@ -18,4 +18,26 @@ def encrypt(text, shift):
     return encrypted_text
 
 encrypted_text = encrypt(text,shift)
-print(encrypted_text)
+
+def decrypt(text, shift):
+    decrypted_text =""
+    for char in text:
+        if char in alphabet:
+            index = alphabet.index(char)
+            decrypt_index = (index - shift) % 26
+            decrypted_char = alphabet[decrypt_index]
+            decrypted_text += decrypted_char
+        else:
+            decrypted_text += char
+    return decrypted_text
+
+decrypted_text = decrypt(text, shift)
+
+if direction == "encode":
+    encrypt(text, shift)
+    print(encrypted_text)
+elif direction == "decode":
+    decrypt(text, shift)
+    print(decrypted_text)
+else:
+    print("Invalid option.")
