7aeb032fc1f7ec9b81eefc452b1484100c884ec7
JayAkagi <b019782k@gmail.com>
2023-12-07 23:16:49 +0000

    Caesar cipher 0.3

--- day8/main.py
+++ day8/main.py
@@ -1,43 +1,29 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+app_running = True
 
-def encrypt(text, shift):
-    encrypted_text = ""
-    for char in text:
-        if char in alphabet:
-            index = alphabet.index(char)
-            encrypted_index = (index + shift) % 26
-            encrypted_char = alphabet[encrypted_index]
-            encrypted_text += encrypted_char
-        else:
-            encrypted_text += char
-    return encrypted_text
+while app_running:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    if direction == "exit":
+        app_running = False
+    else:
+        text = input("Type your message:\n").lower()
+        shift = int(input("Type the shift number:\n"))
 
-encrypted_text = encrypt(text,shift)
 
-def decrypt(text, shift):
-    decrypted_text =""
-    for char in text:
-        if char in alphabet:
-            index = alphabet.index(char)
-            decrypt_index = (index - shift) % 26
-            decrypted_char = alphabet[decrypt_index]
-            decrypted_text += decrypted_char
-        else:
-            decrypted_text += char
-    return decrypted_text
+        def caesar(text, shift):
+            secret_message = ""
+            for char in text:                   
+                if char in alphabet:
+                    index = alphabet.index(char)
+                    if direction == "encode":
+                        new_char = (index + shift) % 26
+                    elif direction == "decode":
+                        new_char = (index - shift) % 26
 
-decrypted_text = decrypt(text, shift)
+                secret_message += alphabet[new_char]
+            print(f"The {direction}d text is: {secret_message}")
 
-if direction == "encode":
-    encrypt(text, shift)
-    print(encrypted_text)
-elif direction == "decode":
-    decrypt(text, shift)
-    print(decrypted_text)
-else:
-    print("Invalid option.")
+        caesar(text, shift)
+
